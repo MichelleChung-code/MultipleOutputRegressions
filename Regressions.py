64c6eafcb8ad8afae14a7ed664e9aa686fc0af97
mchung-code <tkd.m.chung@gmail.com>
2021-03-09 22:35:38 -0700

    Some edits and set up for use in an example
    Note, input sample data not commited yet

--- Regressions.py
+++ Regressions.py
@@ -7,9 +7,12 @@ from sklearn.model_selection import RepeatedKFold
 import numpy as np
 # import json
 from pprint import pprint
+import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.metrics import r2_score
 
 # https://machinelearningmastery.com/multi-output-regression-models-with-python/
+# todo add the chained output
 
 dict_reg_type = {'LinearRegression': LinearRegression,
                  'KNeighborsRegressor': KNeighborsRegressor,
@@ -42,7 +45,9 @@ class RunRegression:
         # Evaluate the multi-output regression using k-fold cross-validation
 
         # 10-fold cross-val
-        cv = RepeatedKFold(n_splits=10, n_repeats=3, random_state=1)
+
+        num_splits = 10 if len(self.X) >= 10 else len(self.X)  # defaults to 10 as long as there are at least 10 samples
+        cv = RepeatedKFold(n_splits=num_splits, n_repeats=3, random_state=1)
         n_scores = cross_val_score(self.model, self.X, self.Y, scoring='neg_mean_absolute_error', cv=cv)
 
         return np.abs(n_scores)
@@ -67,7 +72,11 @@ class RunRegression:
                                        'RandomForestRegressor': RunRegression.get_params_output,
                                        'DecisionTreeRegressor': RunRegression.get_params_output}
 
-        return r_sq, y_predict, dict_additional_output_type[self.model_type](model)
+        if isinstance(self.Y, pd.DataFrame):
+            r_sq_indv_ls = [r2_score(self.Y.iloc[:, i], y_predict[:, i]) for i in range(len(self.Y.columns))]
+        else:
+            r_sq_indv_ls = [r2_score(self.Y[:, i], y_predict[:, i]) for i in range(len(self.Y.columns))]
+        return r_sq, r_sq_indv_ls, y_predict, dict_additional_output_type[self.model_type](model)
 
     @staticmethod
     def lin_reg_output(model):
@@ -107,7 +116,10 @@ class RunRegression:
         x_ax = range(len(self.X))
 
         for i in range(num_outputs):
-            plt.plot(x_ax, self.Y[:, i], label='y{}_actual'.format(i + 1))
+            if isinstance(self.Y, pd.DataFrame):
+                plt.plot(x_ax, np.array(self.Y.iloc[:, i]), label='y{}_actual'.format(i + 1))
+            else:
+                plt.plot(x_ax, self.Y[:, i], label='y{}_actual'.format(i + 1))
             plt.plot(x_ax, y_predict[:, i], label='y{}_pred'.format(i + 1))
 
         plt.title(self.model_type)
@@ -117,10 +129,11 @@ class RunRegression:
     def __call__(self):
         """ Run the regression class """
         n_scores = self.evaluate_model()
-        r_sq, y_predict, additional_output = self.run_regression_model()
+        r_sq, r_sq_indv_ls, y_predict, additional_output = self.run_regression_model()
         print(self.model_type)
         print('Mean Absolute Error, mean (std): {:.3f} ({:.3f})'.format(np.mean(n_scores), np.std(n_scores)))
         print('r^2: {:.3f}'.format(r_sq))
+        print('r^2 individual: {}'.format(r_sq_indv_ls))
         print('Predicted y-series: \n{}'.format(y_predict[:10]))
 
         # the below line only works for primative types
---
+++ crystallization_example/run_crystallization_regression.py
@@ -0,0 +1,38 @@
+import os
+from pathlib import Path
+import pandas as pd
+from Regressions import RunRegression
+
+__author__ = 'Michelle Aria Chung'
+
+# For input data schema
+AGITATOR_SPEED = 'AGITATOR_SPEED'
+SEED_CRYSTAL_MASS = 'SEED_CRYSTAL_MASS'
+YIELD = 'YIELD'
+GROWTH_RATE = 'GROWTH_RATE'
+MEAN_DIAMETER = 'MEAN_DIAMETER'
+
+
+class CrystallizationRegression:
+    def __init__(self, input_data_path):
+        self.input_data = pd.read_csv(input_data_path)
+
+        self.X = self.input_data[[AGITATOR_SPEED, SEED_CRYSTAL_MASS]]
+        self.Y = self.input_data[[YIELD, GROWTH_RATE, MEAN_DIAMETER]]
+
+    def plot_original_data(self):
+        # todo generate different x vs y plots, while keeping other x vars constant
+        pass
+
+    def __call__(self):
+        crystallization_linear_regression = RunRegression(self.X, self.Y, 'LinearRegression')
+        crystallization_linear_regression()
+
+        crystallization_random_for_regression = RunRegression(self.X, self.Y, 'RandomForestRegressor')
+        crystallization_random_for_regression()
+
+
+if __name__ == '__main__':
+    input_data_path = 'crystallization_input_data.csv'
+    x = CrystallizationRegression(input_data_path)
+    x()
